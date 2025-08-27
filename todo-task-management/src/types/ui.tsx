2b3def66056c72601d0e2d4b9f434c8f6ec272f7
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2025-08-27 17:13:03 +0330

    add types

---
+++ todo-task-management/src/types/ui.tsx
@@ -0,0 +1,40 @@
+import * as Icons from "lucide-react";
+import React from "react";
+
+export interface ButtonProps {
+  title?: string;
+  onClick?: () => void;
+  link?: string;
+  variant?: "primary" | "secondary" | "outline" | "danger";
+  size?: "sm" | "md" | "lg" | "icon";
+  className?: string;
+  textClassName?: string;
+  disabled?: boolean;
+  loading?: boolean;
+  iconLeft?: React.ReactNode | keyof typeof Icons;
+  iconRight?: React.ReactNode | keyof typeof Icons;
+  fullWidth?: boolean;
+  type?: "button" | "submit" | "reset";
+}
+
+export interface IconProps {
+  name: keyof typeof Icons;
+  size?: "sm" | "md" | "lg" | number;
+  color?: string;
+  label?: string;
+  onClick?: () => void;
+  className?: string;
+  hoverEffect?: boolean;
+}
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: string;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+  secureTextEntry?: boolean;
+  secureToggle?: boolean;
+  containerClassName?: string;
+  inputClassName?: string;
+}
